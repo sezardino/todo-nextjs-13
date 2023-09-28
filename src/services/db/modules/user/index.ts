f829e6e4300b88a193852beedc90ea2149f5dec9
Edward Arechwa <e.arechwa@nebucode.com>
2023-09-28 22:41:02 +0200

    ft: add service for update user settings

--- src/services/db/modules/user/index.ts
+++ src/services/db/modules/user/index.ts
@@ -1,22 +1,35 @@
 import { passwordService } from "@/services/password";
 import { User } from "@prisma/client";
+import { DBModuleError, isDBModuleError } from "../../types";
 import { AbstractDBModule } from "../abstract";
-import { CreateUserDto, FindOneUserDto } from "./types";
+import {
+  CreateUserDto,
+  DeleteUserDto,
+  FindOneUserBaseDto,
+  FindOneUserDto,
+  FindOneUserFoundDto,
+  FindOneUserNotFoundDto,
+  UpdatePasswordDto,
+  UpdateUserDataDto,
+} from "./types";
 
 export class UserDBModule extends AbstractDBModule {
-  async findOne(dto: FindOneUserDto): Promise<User | null | "403" | "404"> {
-    const { login, throwIfFound, throwIfNotFound } = dto;
+  async findOne(dto: FindOneUserBaseDto): Promise<User | null>;
+  async findOne(dto: FindOneUserNotFoundDto): Promise<User | DBModuleError>;
+  async findOne(dto: FindOneUserFoundDto): Promise<null | DBModuleError>;
+  async findOne(dto: FindOneUserDto): Promise<User | null | DBModuleError> {
+    const { login } = dto;
 
     const user = await this.prismaService.user.findUnique({
       where: { login },
     });
 
-    if (user && throwIfFound) {
-      return "403";
+    if (user && "throwIfFound" in dto && dto.throwIfFound === true) {
+      return { code: 403, message: "Forbidden" };
     }
 
-    if (!user && throwIfNotFound) {
-      return "404";
+    if (!user && "throwIfNotFound" in dto && dto.throwIfNotFound === true) {
+      return { code: 404, message: "User not found" };
     }
 
     return user;
@@ -33,5 +46,65 @@ export class UserDBModule extends AbstractDBModule {
     });
 
     return user;
+  }
+
+  async updateData(dto: UpdateUserDataDto): Promise<User | DBModuleError> {
+    const { login, bio, email, image, localization, name, socials } = dto;
+
+    const toUpdate: Record<string, any> = {
+      bio,
+      email,
+      image,
+      localization,
+      name,
+      socials,
+    };
+
+    Object.entries(toUpdate).forEach(([key, value]) => {
+      if (typeof value === "undefined") delete toUpdate[key];
+    });
+
+    const findResponse = await this.findOne({ login, throwIfNotFound: true });
+
+    if (isDBModuleError(findResponse)) return findResponse;
+
+    return await this.prismaService.user.update({
+      where: { login },
+      data: toUpdate,
+    });
+  }
+
+  async delete(dto: DeleteUserDto): Promise<User | DBModuleError> {
+    const { login } = dto;
+
+    const findResponse = await this.findOne({ login, throwIfNotFound: true });
+
+    if (isDBModuleError(findResponse)) return findResponse;
+
+    return await this.prismaService.user.update({
+      where: { login },
+      data: { status: "INACTIVE" },
+    });
+  }
+
+  async updatePassword(dto: UpdatePasswordDto): Promise<User | DBModuleError> {
+    const { login, newPassword, oldPassword } = dto;
+
+    const findResponse = await this.findOne({ login, throwIfNotFound: true });
+
+    if (isDBModuleError(findResponse) || !findResponse)
+      return findResponse as DBModuleError;
+
+    const isPasswordMatch = await passwordService.compare(
+      oldPassword,
+      findResponse.password
+    );
+
+    if (!isPasswordMatch) return { code: 400, message: "Wrong Password" };
+
+    return await this.prismaService.user.update({
+      where: { login },
+      data: { password: await passwordService.hash(newPassword) },
+    });
   }
 }
--- src/services/db/modules/user/types.ts
+++ src/services/db/modules/user/types.ts
@@ -1,10 +1,43 @@
-export type FindOneUserDto = {
+export type FindOneUserBaseDto = {
   login: string;
-  throwIfNotFound?: boolean;
-  throwIfFound?: boolean;
 };
+
+export type FindOneUserFoundDto = {
+  login: string;
+  throwIfFound: true;
+};
+
+export type FindOneUserNotFoundDto = {
+  login: string;
+  throwIfNotFound: true;
+};
+
+export type FindOneUserDto =
+  | FindOneUserBaseDto
+  | FindOneUserFoundDto
+  | FindOneUserNotFoundDto;
 
 export type CreateUserDto = {
   login: string;
   password: string;
+};
+
+export type UpdatePasswordDto = {
+  login: string;
+  oldPassword: string;
+  newPassword: string;
+};
+
+export type UpdateUserDataDto = {
+  login: string;
+  image?: string;
+  bio?: string;
+  email?: string;
+  localization?: string;
+  name?: string;
+  socials?: string[];
+};
+
+export type DeleteUserDto = {
+  login: string;
 };
