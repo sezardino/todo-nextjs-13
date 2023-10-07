a5c24b087f546734aa2583600befe8a01f1c9e24
Edward Arechwa <e.arechwa@nebucode.com>
2023-10-07 11:54:13 +0200

    ft: create todo form modal

--- src/components/forms/TodoForm.tsx
+++ src/components/forms/TodoForm.tsx
@@ -3,11 +3,9 @@ import { Form, FormikProvider, useFormik } from "formik";
 import { type ComponentPropsWithoutRef, type FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { FormikInput } from "../fields/FormikInput";
-import { FormikTextarea } from "../fields/FormikTextarea";
 
 export interface TodoFormValues {
   name: string;
-  description: string;
 }
 
 export interface TodoFormProps extends ComponentPropsWithoutRef<"form"> {
@@ -23,7 +21,6 @@ export const TodoForm: FC<TodoFormProps> = (props) => {
   const formik = useFormik<TodoFormValues>({
     initialValues: {
       name: initialValues?.name ?? "",
-      description: initialValues?.description ?? "",
     },
     onSubmit: onFormSubmit,
   });
@@ -32,8 +29,7 @@ export const TodoForm: FC<TodoFormProps> = (props) => {
     <FormikProvider value={formik}>
       <Form {...rest} className={twMerge("space-y-2", className)}>
         <FormikInput name="name" placeholder="Name" />
-        <FormikTextarea name="description" placeholder="Description" />
-        <div>
+        <div className="flex gap-3 flex-wrap justify-between items-center">
           <Button type="reset" onClick={onCancelClick}>
             Cancel
           </Button>
---
+++ src/components/modules/todo/TodoFormModal.tsx
@@ -0,0 +1,28 @@
+import { Dialog, DialogProps } from "@/components/base/Dialog";
+import { TodoForm, TodoFormProps } from "@/components/forms/TodoForm";
+import { type FC } from "react";
+
+export type TodoFormModalProps = Omit<
+  DialogProps,
+  "footer" | "header" | "body"
+> & {
+  title: string;
+  onFormSubmit: TodoFormProps["onFormSubmit"];
+  initialValues?: TodoFormProps["initialValues"];
+};
+
+export const TodoFormModal: FC<TodoFormModalProps> = (props) => {
+  const { onFormSubmit, initialValues, className, ...rest } = props;
+
+  return (
+    <Dialog {...rest}>
+      <Dialog.Body>
+        <TodoForm
+          initialValues={initialValues}
+          onFormSubmit={onFormSubmit}
+          onCancelClick={rest.onClose}
+        />
+      </Dialog.Body>
+    </Dialog>
+  );
+};
